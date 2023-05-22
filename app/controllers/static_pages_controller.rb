f866d509fa328cb2dd52495449bc78eb8ad7d7ac
Taras <ivanyuk.taras@gmail.com>
2023-05-22 09:26:30 -0500

    Move from SSD

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,15 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+
+  def show
+    begin
+      flickr = Flickr.new ENV['pusher_key'], ENV['pusher_secret']
+      if !params[:id].blank? 
+        @photos = flickr.photos.search(user_id: params[:id])
+      end
+    rescue StandardError 
+      flash[:alert] = "Please try again..."
+      redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :static_pages, only: [:show]
+  root 'static_pages#show'
 end
